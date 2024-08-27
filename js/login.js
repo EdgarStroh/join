f2420272c29727c44efd5052461ed55c095a6538
Anica Gillen <anica.gillen@gmx.de>
2024-08-27 12:13:35 +0200

    changed design sign up
    Co-Authored-By: bastianelm <178935417+bastianelm@users.noreply.github.com>
    Co-Authored-By: EdgarStroh <170419165+EdgarStroh@users.noreply.github.com>

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 function openPopup() {
-  document.getElementById("popupOverlay").style.display = "flex";
+//   document.getElementById("popupOverlay").style.display = "flex";
   document.getElementById("popupModal").style.display = "block";
 }
 
 // Schließe das Pop-up, wenn man auf das Overlay klickt
-function closePopup() {
-  document.getElementById("popupOverlay").style.display = "none";
-  document.getElementById("popupModal").style.display = "none";
-}
+// function closePopup() {
+//   document.getElementById("popupOverlay").style.display = "none";
+//   document.getElementById("popupModal").style.display = "none";
+// }
