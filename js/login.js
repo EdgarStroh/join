ab15863df873de5604e055d5cd85f795501d4804
Anica Gillen <anica.gillen@gmx.de>
2024-08-28 15:42:40 +0200

    designed checkbox for login

--- js/login.js
+++ js/login.js
@@ -8,3 +8,23 @@ function closePopup() {
 //   document.getElementById("popupOverlay").style.display = "none";
   document.getElementById("popupModal").style.display = "none";
 }
+
+
+
+// Variable, um den aktuellen Zustand zu speichern
+let isChecked = false;
+
+// Toggle-Funktion zum Umschalten des Checkbox-Zustands
+function toggleCheckbox() {
+    const checkboxLabel = document.getElementById('checkboxLabel');
+
+    // Zustand umschalten
+    isChecked = !isChecked;
+
+    // Je nach Zustand das passende Bild setzen
+    if (isChecked) {
+        checkboxLabel.style.backgroundImage = "url('../assets/icons/checkbox_checked.svg')"; // Angekreuzte Checkbox
+    } else {
+        checkboxLabel.style.backgroundImage = "url('../assets/icons/checkbox.svg')"; // Nicht angekreuzte Checkbox
+    }
+}
