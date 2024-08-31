8b0c5ec3b446a7c4d1cc9612d6d1dbdafb3d4d96
bastianelm <elmbastian@proton.me>
2024-08-31 20:22:48 +0200

    added urgent.svg again, solved some problems with prio buttons

--- js/add_task.js
+++ js/add_task.js
@@ -1,15 +1,29 @@
-const prioButtons = document.querySelectorAll('#prio button');
+let prioButtons = document.querySelectorAll('#prio button');
 
-for(i= 0; i<prioButtons.length; i++){
-    prioButtons[i].addEventListener('click', (event)=>{
+for (let i = 0; i < prioButtons.length; i++) {
+    prioButtons[i].addEventListener('click', (event) => {
         const selectedButton = event.currentTarget;
-        if(selectedButton.classList.contains('activePrio') === false){
-            for(const button of prioButtons){
-                if(button.classList.contains('activePrio')){
-                    button.classList.remove('activePrio')
-                };
-            }
-            selectedButton.classList.add('activePrio');
+        const activePrioClass = selectedButton.getAttribute('id') + 'Active';
+        const activeButtonBool = selectedButton.classList.contains(activePrioClass);
+
+        if (!activeButtonBool) {
+            // Remove the active class from all buttons
+            prioButtons.forEach(button => {
+                const buttonActiveClass = button.getAttribute('id') + 'Active';
+                if (button.classList.contains(buttonActiveClass)) {
+                    button.classList.remove(buttonActiveClass);
+                    console.log("active prio class should be removed");
+                    document.getElementById(button.getAttribute('id') + 'Img').src = '../assets/icons/' + button.getAttribute('id') + '.svg';
+                }
+            });
+
+            // Add active class to the selected button
+            selectedButton.classList.add(activePrioClass);
+            document.getElementById(selectedButton.getAttribute('id') + 'Img').src = '../assets/icons/' + selectedButton.getAttribute('id') + 'Selected.svg';
+        } else {
+            // Remove active class if the same button is clicked again
+            selectedButton.classList.remove(activePrioClass);
+            document.getElementById(selectedButton.getAttribute('id') + 'Img').src = '../assets/icons/' + selectedButton.getAttribute('id') + '.svg';
         }
     });
 }
