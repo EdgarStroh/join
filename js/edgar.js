aa16f51d0b73878ffe9892b945a27173163989c9
EdgarStroh <edgarstroh@googlemail.com>
2024-08-31 16:20:20 +0200

    console.log

--- js/edgar.js
+++ js/edgar.js
@@ -8,9 +8,9 @@ const BASE_URL_Contact = "https://join-b197b-default-rtdb.europe-west1.firebased
 
 // load for contacts
 async function loadDataContacts(path=""){
-    console.log("Contacts:");
     let response = await fetch(BASE_URL_Contact + path + ".json");
     let responseToJson = await response.json();
+    console.log("Contacts:");
     console.log(responseToJson);
 }
 
@@ -19,9 +19,9 @@ const BASE_URL_Board = "https://join-b197b-default-rtdb.europe-west1.firebasedat
 
 // load for task for board
 async function loadDataBoards(path=""){
-    console.log("Task's for Board:");
     let response = await fetch(BASE_URL_Board + path + ".json");
     let responseToJson = await response.json();
+    console.log("Task's for Board:");
     console.log(responseToJson);
 }
 
@@ -30,8 +30,8 @@ const BASE_URL_USERS = "https://join-b197b-default-rtdb.europe-west1.firebasedat
 
 // load for task for board
 async function loadDataUsers(path=""){
-    console.log("Users:");
     let response = await fetch(BASE_URL_USERS + path + ".json");
     let responseToJson = await response.json();
+    console.log("Users:");
     console.log(responseToJson);
 }
