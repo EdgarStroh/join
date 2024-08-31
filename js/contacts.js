246f6533b236d5043658cbe9b819e4c2b5d883e7
Anica Gillen <anica.gillen@gmx.de>
2024-08-31 21:45:29 +0200

    added slidIn for contacts

---
+++ js/contacts.js
@@ -0,0 +1,5 @@
+function openContact(){
+    document.getElementById("extended_contact").classList.remove('hidden');
+    document.getElementById("extended_contact").classList.add("slideIn");
+   document.getElementById("extended_contact").classList.add("show");
+}
