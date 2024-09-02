2f1f41007e0edb7a4f0d80859466706a6dd9d21e
Anica Gillen <anica.gillen@gmx.de>
2024-09-02 12:15:58 +0200

    Update contacts.js
    Co-Authored-By: EdgarStroh <170419165+EdgarStroh@users.noreply.github.com>

--- js/contacts.js
+++ js/contacts.js
@@ -2,7 +2,7 @@ function openContact() {
     document.getElementById("extended_contact").classList.remove('hidden');
     document.getElementById("extended_contact").classList.add("slideIn");
     document.getElementById("extended_contact").classList.add("show");
-    renderExtendedContact(name, email, phone);
+    renderExtendedContact(email, phone);
 }
 
 function openPopup() {
