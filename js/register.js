2b5d68532510b58c2af2664f4dfa05e7a0c79214
EdgarStroh <edgarstroh@googlemail.com>
2024-09-02 20:44:42 +0200

    fixed few problems but not all

--- js/register.js
+++ js/register.js
@@ -1,30 +1,35 @@
 function signUpUser() {
+    // Verhindert das Standard-Formularabsendeverhalten
+    event.preventDefault();
+
     // Eingabewerte aus dem Formular abrufen
     let userColor = getRandomColor();
-    let email = document.getElementById("inputEmail");
-    let name = document.getElementById("inputName");
-    let password = document.getElementById("inputPassword");
-    let passwordConfirm = document.getElementById("inputConfirmPassword");
-  
+    let email = document.getElementById("inputEmail").value;
+    let name = document.getElementById("inputName").value;
+    let password = document.getElementById("inputPassword").value;
+
     // Daten für den Benutzer vorbereiten, einschließlich der Farbe
     let UserData = {
-        "color": userColor ,
-        "email": email.value,
-        "name": name.value,
-        "password": password.value
+        "color": userColor,
+        "email": email,
+        "name": name,
+        "password": password
     };
 
     // Funktion aufrufen, um die Daten zu posten
     postDataUsers("", UserData);
 
     // Eingabefelder zurücksetzen
-    name.value = "";
-    email.value = "";
-    password.value = "";
-    passwordConfirm.value="";
+    document.getElementById("inputName").value = "";
+    document.getElementById("inputEmail").value = "";
+    document.getElementById("inputPassword").value = "";
+    document.getElementById("inputConfirmPassword").value = "";
 
     // Popup schließen (falls vorhanden)
     closePopup();
+
+    // Gibt true zurück, damit das Formular nicht erneut abgesendet wird
+    return false;
 }
 
 function check() {
