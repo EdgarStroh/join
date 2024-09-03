e2fe1be3dc082ac1bdc327e6b4d7838ce991529a
EdgarStroh <edgarstroh@googlemail.com>
2024-09-03 15:15:49 +0200

    added succes register pop up

--- js/register.js
+++ js/register.js
@@ -2,19 +2,17 @@ function signUpUser(event) {
     const checkbox = document.getElementById('checkboxLogin1');
     if (!checkbox.checked) {
         alert('Please accept the Privacy policy to sign up.');
-        event.preventDefault(); // Prevent form submission
-        return false; // Return false to prevent form submission
+        event.preventDefault();
+        return false;
     }
-    // Verhindert das Standard-Formularabsendeverhalten
+    
     event.preventDefault();
 
-    // Eingabewerte aus dem Formular abrufen
     let userColor = getRandomColor();
     let email = document.getElementById("inputEmail").value;
     let name = document.getElementById("inputName").value;
     let password = document.getElementById("inputPassword").value;
 
-    // Daten für den Benutzer vorbereiten, einschließlich der Farbe
     let UserData = {
         "color": userColor,
         "email": email,
@@ -22,16 +20,15 @@ function signUpUser(event) {
         "password": password
     };
 
-    // Funktion aufrufen, um die Daten zu posten
     postDataUsers("", UserData);
 
-    // Eingabefelder zurücksetzen
     document.getElementById("inputName").value = "";
     document.getElementById("inputEmail").value = "";
     document.getElementById("inputPassword").value = "";
     document.getElementById("inputConfirmPassword").value = "";
     document.getElementById("sign_up_button_div").classList.remove("displayNone");
     closePopup();
+    showPopup();
 }
 
 function check() {
@@ -41,4 +38,18 @@ function check() {
     } else {
         input.setCustomValidity('');  // Korrekt: Nachricht wird entfernt, wenn Passwörter übereinstimmen
     }
+}
+
+function showPopup() {
+    document.getElementById('popupOverlay').style.display = 'flex';
+    document.getElementById('popupSuccess').style.display = 'flex';
+
+    setTimeout(function() {
+        closePopupSuccess();
+    }, 800);
+}
+
+function closePopupSuccess() {
+    document.getElementById('popupOverlay').style.display = 'none';
+    document.getElementById('popupSuccess').style.display = 'none';
 }
