5e6cafdfb9eb0a91c4440803c8911fd40811b77b
EdgarStroh <edgarstroh@googlemail.com>
2024-09-09 09:38:50 +0200

    removed old drag and drop logic

--- js/board.js
+++ js/board.js
@@ -1,20 +1,20 @@
-let todos = [{
-    'id': 0,
-    'title': 'Putzen',
-    'category': 'toDo'
-}, {
-    'id': 1,
-    'title': 'Kochen',
-    'category': 'inProgress'
-}, {
-    'id': 2,
-    'title': 'Einkaufen',
-    'category': 'done'
-},{
-    'id': 3,
-    'title': 'Duschen',
-    'category': 'awaitFeedback'
-}];
+// let todos = [{
+//     'id': 0,
+//     'title': 'Putzen',
+//     'category': 'toDo'
+// }, {
+//     'id': 1,
+//     'title': 'Kochen',
+//     'category': 'inProgress'
+// }, {
+//     'id': 2,
+//     'title': 'Einkaufen',
+//     'category': 'done'
+// },{
+//     'id': 3,
+//     'title': 'Duschen',
+//     'category': 'awaitFeedback'
+// }];
 
 let currentDraggedElement;
 let currentDraggedElementIndex;
@@ -27,59 +27,73 @@ function updateAll() {
     renderBoardList();
 }
 
-function updateToDo() {
-    let toDo = todos.filter(t => t['category'] == 'toDo');
-    document.getElementById('toDo').innerHTML = '';
-    for (let index = 0; index < toDo.length; index++) {
-        const element = toDo[index];
-        document.getElementById('toDo').innerHTML += generateBoardContent(element);
-    }
-}
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
 
-function updateInProgress() {
-    let inProgress = todos.filter(t => t['category'] == 'inProgress');
-    document.getElementById('inProgress').innerHTML = '';
-    for (let index = 0; index < inProgress.length; index++) {
-        const element = inProgress[index];
-        document.getElementById('inProgress').innerHTML += generateBoardContent(element);
-    }
-}
+// function updateToDo() {
+//     let toDo = todos.filter(t => t['category'] == 'toDo');
+//     document.getElementById('toDo').innerHTML = '';
+//     for (let index = 0; index < toDo.length; index++) {
+//         const element = toDo[index];
+//         document.getElementById('toDo').innerHTML += generateBoardContent(element);
+//     }
+// }
 
-function updateAwaitFeedback() {
-    let awaitFeedback = todos.filter(t => t['category'] == 'awaitFeedback');
-    document.getElementById('awaitFeedback').innerHTML = '';
-    for (let index = 0; index < awaitFeedback.length; index++) {
-        const element = awaitFeedback[index];
-        document.getElementById('awaitFeedback').innerHTML += generateBoardContent(element);
-    }
-}
+// function updateInProgress() {
+//     let inProgress = todos.filter(t => t['category'] == 'inProgress');
+//     document.getElementById('inProgress').innerHTML = '';
+//     for (let index = 0; index < inProgress.length; index++) {
+//         const element = inProgress[index];
+//         document.getElementById('inProgress').innerHTML += generateBoardContent(element);
+//     }
+// }
 
-function updateDone() {
-    let done = todos.filter(t => t['category'] == 'done');
-    document.getElementById('done').innerHTML = '';
-    for (let index = 0; index < done.length; index++) {
-        const element = done[index];
-        document.getElementById('done').innerHTML += generateBoardContent(element);
-    }
-}
+// function updateAwaitFeedback() {
+//     let awaitFeedback = todos.filter(t => t['category'] == 'awaitFeedback');
+//     document.getElementById('awaitFeedback').innerHTML = '';
+//     for (let index = 0; index < awaitFeedback.length; index++) {
+//         const element = awaitFeedback[index];
+//         document.getElementById('awaitFeedback').innerHTML += generateBoardContent(element);
+//     }
+// }
 
-function startDragging(id) {
-    currentDraggedElement = id;
-}
+// function updateDone() {
+//     let done = todos.filter(t => t['category'] == 'done');
+//     document.getElementById('done').innerHTML = '';
+//     for (let index = 0; index < done.length; index++) {
+//         const element = done[index];
+//         document.getElementById('done').innerHTML += generateBoardContent(element);
+//     }
+// }
+
+// function startDragging(id) {
+//     currentDraggedElement = id;
+// }
 
 
 // function generateTodoHTML(element) {
 //     return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
 // }
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+// function allowDrop(ev) {
+//     ev.preventDefault();
+// }
 
-function moveTo(category) {
-    todos[currentDraggedElement]['category'] = category;
-    updateAll();
-}
+// function moveTo(category) {
+//     todos[currentDraggedElement]['category'] = category;
+//     updateAll();
+// }
 
 function highlight(id) {
     document.getElementById(id).classList.add('drag-area-highlight');
@@ -87,6 +101,16 @@ function highlight(id) {
 
 function removeHighlight(id) {
     document.getElementById(id).classList.remove('drag-area-highlight');
+}
+
+function renderBoardList() {
+    let boardContainer = document.getElementById("toDo");
+    boardContainer.innerHTML = ""; // Clear any existing content
+
+    // Generate and insert the board content into the container
+    allBoardContent.forEach((board, index) => {
+        boardContainer.innerHTML += generateBoardContent(board, index); // Pass index as argument
+    });
 }
 
 function openPopup() {
@@ -117,14 +141,4 @@ function closePopup() {
     setTimeout(() => {
         popupModal.style.display = 'none';
     }, 120); // 120ms entspricht der Dauer der Animation
-}
-
-function renderBoardList() {
-    let boardContainer = document.getElementById("toDo");
-    boardContainer.innerHTML = ""; // Clear any existing content
-
-    // Generate and insert the board content into the container
-    allBoardContent.forEach((board, index) => {
-        boardContainer.innerHTML += generateBoardContent(board, index); // Pass index as argument
-    });
 }
