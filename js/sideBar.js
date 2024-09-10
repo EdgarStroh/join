2c0ac4679b1cd32c263eaca65136c9f9d42f4b22
bastianelm <elmbastian@proton.me>
2024-09-10 13:53:21 +0200

    Added activeLinks for Sidebar

---
+++ js/sideBar.js
@@ -0,0 +1,9 @@
+let currentSite = window.location.pathname;
+let navigationLinks = document.querySelectorAll('.sideBar a');
+console.log(navigationLinks);
+for(let i = 0; i < navigationLinks.length; i++){
+    let link = navigationLinks[i];
+    if(link.href.includes(currentSite)){
+        link.classList.add('activeNavigationPoint');
+    }
+}
