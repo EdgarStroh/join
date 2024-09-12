002c68c87667cc79226d09f74e1e8204888e2c65
Anica Gillen <anica.gillen@gmx.de>
2024-09-12 08:31:10 +0200

    refactoring header.js

--- js/header.js
+++ js/header.js
@@ -3,25 +3,9 @@ window.addEventListener("load", () => {
   let headerSubMenu = document.getElementById("headerSubMenu");
 
   if (profileName && headerSubMenu) {
-    // Funktion zum Zurückgeben der Initialen
-    function returnNameInitials() {
-      let userName = localStorage.getItem("loggedInUser") || "Guest";
-      let initials = userName.substring(0, 1); // Standardmäßig den ersten Buchstaben
+    let userName = localStorage.getItem("loggedInUser") || "Guest";
 
-      let spacePosition = userName.indexOf(" ");
-
-      // Falls es einen Nachnamen gibt, nimm den ersten Buchstaben des Nachnamens
-      if (spacePosition !== -1) {
-        initials += userName.substring(spacePosition + 1, spacePosition + 2);
-      }
-
-      return initials.toUpperCase(); // Initialen immer in Großbuchstaben zurückgeben
-    }
-
-    // Setze die Initialen im Profilnamen
-    profileName.innerHTML = returnNameInitials();
-
-    // Füge den Event-Listener hinzu, um das Menü ein- oder auszublenden
+    profileName.innerHTML = getInitials(userName).toUpperCase();
     profileName.addEventListener("click", () => {
       headerSubMenu.classList.toggle("hidden");
     });
@@ -29,3 +13,10 @@ window.addEventListener("load", () => {
     console.error("Profilname oder Menü nicht gefunden.");
   }
 });
+
+function toggleMenu() {
+  let menu = document.getElementById("headerSubMenu");
+  if (menu) {
+    menu.classList.toggle("hidden");
+  }
+}
