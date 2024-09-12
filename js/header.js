131d41869a0985fc8d4743278eb78488cd5ccf6b
Anica Gillen <anica.gillen@gmx.de>
2024-09-12 07:33:26 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,31 +1,20 @@
-// window.addEventListener('load', ()=>{
-//     let profileName = document.getElementById('profileName');
-//     let headerSubMenu = document.getElementById('headerSubMenu');
-//     function returnNameInitials(){
-//         let userName = localStorage.getItem('inputName') || "Guest";
-//         let string;
-//         if(userName === undefined){
-//             string = userName.substring(0,1);
-//         } else{
-//             let spacePosition = userName.indexOf(' ');
-//             string = userName.substring(spacePosition+1, spacePosition+2);
-//         }
-//         return string;
-//     }
-//     profileName.innerHTML = returnNameInitials();
-//     profileName.addEventListener('click', ()=>{
-//         headerSubMenu.classList.toggle('hidden');
-//     })
-// });
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.addEventListener("click", (event) => {
-    if (event.target && event.target.id === "profileName") {
-      let headerSubMenu = document.getElementById("headerSubMenu");
-      if (headerSubMenu) {
-        headerSubMenu.classList.toggle("hidden");
-      }
+window.addEventListener('load', ()=>{
+    let profileName = document.getElementById('profileName');
+    let headerSubMenu = document.getElementById('headerSubMenu');
+    function returnNameInitials(){
+        let userName = localStorage.getItem('inputName') || "Guest";
+        let string;
+        if(userName === undefined){
+            string = userName.substring(0,1);
+        } else{
+            let spacePosition = userName.indexOf(' ');
+            string = userName.substring(spacePosition+1, spacePosition+2);
+        }
+        return string;
     }
-  });
+    profileName.innerHTML = returnNameInitials();
+    profileName.addEventListener('click', ()=>{
+        headerSubMenu.classList.toggle('hidden');
+    })
 });
 
