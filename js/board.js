1001fd87c74dd81cd2a39768741a27311cb116b6
EdgarStroh <edgarstroh@googlemail.com>
2024-09-16 14:22:51 +0200

    123

--- js/board.js
+++ js/board.js
@@ -363,3 +363,39 @@ async function updateTask(id, data) {
     alert("Es gab ein Problem beim Aktualisieren der Task.");
   }
 }
+
+// PROTON SCHEIß
+//prio buttons
+
+let priority = "";
+
+let prioButtons = document.querySelectorAll('#prio button');
+
+for (let i = 0; i < prioButtons.length; i++) {
+    prioButtons[i].addEventListener('click', (event) => {
+        const selectedButton = event.currentTarget;
+        priority = event.currentTarget.value;
+        const activePrioClass = selectedButton.getAttribute('id') + 'Active';
+        const activeButtonBool = selectedButton.classList.contains(activePrioClass);
+
+        if (!activeButtonBool) {
+            // Remove the active class from all buttons
+            prioButtons.forEach(button => {
+                const buttonActiveClass = button.getAttribute('id') + 'Active';
+                if (button.classList.contains(buttonActiveClass)) {
+                    button.classList.remove(buttonActiveClass);
+                    console.log("active prio class should be removed");
+                    document.getElementById(button.getAttribute('id') + 'Img').src = '../assets/icons/' + button.getAttribute('id') + '.svg';
+                }
+            });
+
+            // Add active class to the selected button
+            selectedButton.classList.add(activePrioClass);
+            document.getElementById(selectedButton.getAttribute('id') + 'Img').src = '../assets/icons/' + selectedButton.getAttribute('id') + 'Selected.svg';
+        } else {
+            // Remove active class if the same button is clicked again
+            selectedButton.classList.remove(activePrioClass);
+            document.getElementById(selectedButton.getAttribute('id') + 'Img').src = '../assets/icons/' + selectedButton.getAttribute('id') + '.svg';
+        }
+    });
+}
