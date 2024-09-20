2f33410fc7b24a681863af9b478a8bf9d8f34219
Anica Gillen <anica.gillen@gmx.de>
2024-09-20 10:44:44 +0200

    logout

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,9 +1,14 @@
 let currentSite = window.location.pathname;
 let navigationLinks = document.querySelectorAll('.sideBar a');
-console.log(navigationLinks);
+// console.log(navigationLinks);
+
 for(let i = 0; i < navigationLinks.length; i++){
     let link = navigationLinks[i];
     if(link.href.includes(currentSite)){
         link.classList.add('activeNavigationPoint');
     }
+}
+
+function logout(){
+    localStorage.clear();
 }
