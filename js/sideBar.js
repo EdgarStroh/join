9db68a49b4fe2c3e1b4fc5d7c7f3c0e4c57c4e60
EdgarStroh <edgarstroh@googlemail.com>
2024-09-30 13:33:13 +0200

    sidebar fix

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,14 +1,14 @@
-let currentSite = window.location.pathname;
-let navigationLinks = document.querySelectorAll('.sideBar a');
-// console.log(navigationLinks);
+document.addEventListener('DOMContentLoaded', function() {
+    w3.includeHTML(function() {
+        let currentSite = window.location.pathname.toLowerCase().replace(/\/$/, '');
+        let navigationLinks = document.querySelectorAll('.sideBar a');
 
-for(let i = 0; i < navigationLinks.length; i++){
-    let link = navigationLinks[i];
-    if(link.href.includes(currentSite)){
-        link.classList.add('activeNavigationPoint');
-    }
-}
-
-function logout(){
-    localStorage.clear();
-}
+        for(let i = 0; i < navigationLinks.length; i++) {
+            let link = navigationLinks[i];
+            let linkPath = new URL(link.href).pathname.toLowerCase().replace(/\/$/, '');
+            if(linkPath === currentSite) {
+                link.classList.add('activeNavigationPoint');
+            }
+        }
+    });
+});
