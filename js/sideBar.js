faf08e5d5b248b1a486b9c8f660c18cdc62c062d
EdgarStroh <edgarstroh@googlemail.com>
2024-09-30 14:29:10 +0200

    Update sideBar.js

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     w3.includeHTML(function () {
         let currentSite = window.location.pathname.toLowerCase().replace(/\/$/, '');
-        console.log(`Current site: ${currentSite}`);
+        // console.log(`Current site: ${currentSite}`);
 
         // Select all sidebar links (both normal and mobile)
         let allLinks = document.querySelectorAll('.sideBar a, .mobileSideBar a');
@@ -9,15 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
         for (let i = 0; i < allLinks.length; i++) {
             let link = allLinks[i];
             let linkPath = new URL(link.href).pathname.toLowerCase().replace(/\/$/, '');
-            console.log(`Checking link: ${linkPath}`);
+            // console.log(`Checking link: ${linkPath}`);
 
             if (linkPath === currentSite) {
                 if (link.closest('.sideBar')) {
                     link.classList.add('activeNavigationPoint');
-                    console.log(`Active Navigation Point added to: ${link.href}`);
+                    // console.log(`Active Navigation Point added to: ${link.href}`);
                 } else if (link.closest('.mobileSideBar')) {
                     link.classList.add('activeMobileNavigationPoint');
-                    console.log(`Active Mobile Navigation Point added to: ${link.href}`);
+                    // console.log(`Active Mobile Navigation Point added to: ${link.href}`);
                 }
             }
         }
